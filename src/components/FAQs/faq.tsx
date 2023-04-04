6bcdc35417b30c3c67542d0b9d94946f738fadd2
Marketing_Jenkins <mktdev@connexuscu.org>
2023-04-04 09:50:23 -0500

    3 Updates

--- src/components/FAQs/faq.tsx
+++ src/components/FAQs/faq.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { client, FaqIdType } from 'client';
+//import { client, FaqIdType } from 'client';
 
 export interface Props {
   id: string;
@@ -8,15 +8,15 @@ export interface Props {
 function FAQ({
   id,
 }: Props): JSX.Element {
-  const { useQuery } = client;
+  /*const { useQuery } = client;
   const faqItem = useQuery().faqBy({
     faqId: parseInt(id)
-  });
+  });*/
   return (
     <div id={id}>
-      <div>Question: {faqItem.title()}</div>
-      <div dangerouslySetInnerHTML={{__html: faqItem.answer}}></div>
-    </div>
+      <div>Question: </div>
+      <div dangerouslySetInnerHTML={{__html: ''}}></div>
+  </div>
   );
 }
 
