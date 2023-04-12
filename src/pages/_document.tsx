b63a75673d7a9eb3664aed2fd09ead682ce9be03
Marketing_Jenkins <mktdev@connexuscu.org>
2023-04-12 10:49:13 -0500

    43 Updates

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ class CXDoc extends Document {
     return (
       <Html>
         <Head>
+          <link rel="stylesheet" type="text/css" href={`https://cloud.typography.com/6914618/${process.env.CLOUD_FONT || '7711232'}/css/fonts.css`} />
         </Head>
         <body className="home page-template page-template-templates page-template-full-width page-template-templatesfull-width-php page page-id-75 wp-custom-logo wp-embed-responsive group-blog featured-image-wide">
           <Main />
