88c212e79b186c9026a65a72340d1a6d3ca9bbb1
Marketing_Jenkins <mktdev@connexuscu.org>
2023-05-03 21:18:01 -0500

    76 Updates

--- src/components/Navigation/Navbar.tsx
+++ src/components/Navigation/Navbar.tsx
@@ -52,7 +52,7 @@ function DesktopHeaderNavigation(props: HeaderNavigationProps) {
                                                     {title?.childItems()?.nodes?.map((navLink, index) => {
                                                         return (
                                                             <li key={`${navLink.id}-${navLink.databaseId}`}>
-                                                                <Link href={navLink.uri || ''} passHref className='dropdown-item cx-nav__dropdown-item'
+                                                                <Link href={navLink.uri || ''} passHref prefetch={false} className='dropdown-item cx-nav__dropdown-item'
                                                                     onClick={() => {
                                                                         setIsNavExpanded(!isNavExpanded);
                                                                         setNavSelected('');
