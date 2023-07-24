00b4bbaec2c81df8f43b2f3ba1f26103ebfb4e17
Marketing_Jenkins <mktdev@connexuscu.org>
2023-07-24 15:20:32 -0500

    141 Updates

--- src/pages/locations/index.tsx
+++ src/pages/locations/index.tsx
@@ -3,7 +3,11 @@ import { client } from "client";
 import { Footer, Header } from "components";
 import { GetStaticPropsContext } from "next";
 import Head from "next/head";
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect } from "react";
+import {
+	showDetailsContext,
+	selectedLocationContext,
+} from "components/Locations/locationsContext";
 import LocationListings from "components/Locations/listings";
 import LocationDetails from "components/Locations/location-details";
 import GTM from "components/ThirdParty/gtm";
@@ -20,14 +24,15 @@ import HotJar from "components/ThirdParty/hotjar";
 import Qualtrics from "components/ThirdParty/qualtrics";
 import Spectrum from "components/ThirdParty/spectrum";
 
-export default function Page({locationSettings}) {
+export default function Page({ locationSettings }) {
 	const { useQuery } = client;
 	const { generalSettings } = useQuery();
 
 	const [data, setData] = useState(null);
 	const [length, setLength] = useState(null);
 	const [isLoading, setLoading] = useState(false);
-
+	const [showDetails, setShowDetails] = useState(false);
+	const [selectedLocation, setSelectedLocation] = useState(null);
 
 	useEffect(() => {
 		setLoading(true);
@@ -37,7 +42,7 @@ export default function Page({locationSettings}) {
 			.then((res) => res.json())
 			.then((data) => {
 				setData(data);
-        setLength(Object.keys(data).length);
+				setLength(Object.keys(data).length);
 				setLoading(false);
 			});
 	}, []);
@@ -56,48 +61,46 @@ export default function Page({locationSettings}) {
 			<GTM />
 			<HotJar />
 
-			<main className="content content-single">
-				<div className="wrap">
-					<Container align="full" classNames={`no-margin`}>
-						<Columns classNames={`no-margin`}>
-							<Column>
-								<PageTitle heading="Locations & ATMs" />
-							</Column>
-						</Columns>
-					</Container>
-					<div id="wpsl-wrap" className="wpsl-wrap wpsl-store-below wpsl-default-filters">
-						<div className="wpsl-search wpsl-clearfix wpsl-checkboxes-enabled wpsl-geolocation-run"></div>
-						<Wrapper apiKey={locationSettings.apiBrowserKey}>
-							<Map lat={45} lng={-89} locationSettings={locationSettings} />
-						</Wrapper>
-						{/* <div id="wpsl-gmap" className="wpsl-gmap-canvas" style={{ position: "relative", overflow: "hidden" }}></div> */}
-						<div id="wpsl-result-list">
-							<div id="wpsl-stores">
-								<div className="cx-location-listing__title wpsl-location--section">
-									<em>
-										<small>
-											<span id="store-count">{length}</span>&nbsp;results
-										</small>
-									</em>
+			<showDetailsContext.Provider value={{ showDetails, setShowDetails }}>
+				<selectedLocationContext.Provider
+					value={{ selectedLocation, setSelectedLocation }}
+				>
+					<main className="content content-single">
+						<div className="wrap">
+							<Container align="full" classNames={`no-margin`}>
+								<Columns classNames={`no-margin`}>
+									<Column>
+										<PageTitle heading="Locations & ATMs" />
+									</Column>
+								</Columns>
+							</Container>
+							<div
+								id="wpsl-wrap"
+								className="wpsl-wrap wpsl-store-below wpsl-default-filters"
+							>
+								<div className="wpsl-search wpsl-clearfix wpsl-checkboxes-enabled wpsl-geolocation-run"></div>
+								<Wrapper apiKey={locationSettings.apiBrowserKey}>
+									<Map lat={45} lng={-89} locationSettings={locationSettings} />
+								</Wrapper>
+								{/* <div id="wpsl-gmap" className="wpsl-gmap-canvas" style={{ position: "relative", overflow: "hidden" }}></div> */}
+								<div id="wpsl-result-list">
+									<div id="wpsl-stores">
+										<div className="cx-location-listing__title wpsl-location--section">
+											<em>
+												<small>
+													<span id="store-count">{length}</span>&nbsp;results
+												</small>
+											</em>
+										</div>
+										<LocationListings data={data} />
+									</div>
 								</div>
-								<LocationListings data={data} />
+								<LocationDetails />
 							</div>
 						</div>
-						<LocationDetails />
-					</div>
-
-					<Container classNames={`no-margin`}>
-						<Columns classNames={`no-margin`}>
-							<Column>
-								First Column
-							</Column>
-							<Column>
-								Second Column
-							</Column>
-						</Columns>
-					</Container>
-				</div>
-			</main>
+					</main>
+				</selectedLocationContext.Provider>
+			</showDetailsContext.Provider>
 
 			<Footer copyrightHolder={generalSettings.title} />
 			<Qualtrics />
@@ -108,20 +111,20 @@ export default function Page({locationSettings}) {
 
 export async function getStaticProps(context: GetStaticPropsContext) {
 	const { data } = await apolloClient.query({
-        query: gql`
-		${LocationSettingsFragment}
-        query LocationSettings {
-			locationSettings {
-				...LocationSettingsFragment
+		query: gql`
+			${LocationSettingsFragment}
+			query LocationSettings {
+				locationSettings {
+					...LocationSettingsFragment
+				}
 			}
-          }
-      `,
-    });
+		`,
+	});
 	return getNextStaticProps(context, {
 		Page,
 		client,
-        props: {
-			locationSettings: data.locationSettings
-        }
+		props: {
+			locationSettings: data.locationSettings,
+		},
 	});
 }
