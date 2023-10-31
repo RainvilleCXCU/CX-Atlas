62184e5d37d03ae0fa1cb1b6dc269a797d1b6a91
Marketing_Jenkins <mktdev@connexuscu.org>
2023-10-31 16:09:43 -0500

    197 Updates

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -5,19 +5,19 @@ interface Props {
 
 const Calculator = ({ calculatorName }: Props): JSX.Element => {
 	useEffect(() => {
-		import("./Dinkytown/Core/KJE.js").then(({ KJE }) => {
-			window.KJE = Object(KJE);
-			let bForceWidth;
-			let curtop;
-			import(`./Dinkytown/Site/KJESiteSpecific.js`).then(() => {
-				import(`./Dinkytown/Core/${calculatorName}.js`).then(() => {
-					import(`./Dinkytown/Site/${calculatorName}Params.js`).then(() => {
-						KJE.init();
+		if(!window.KJE) {
+			import("./Dinkytown/Core/KJE.js").then(({ KJE }) => {
+				window.KJE = Object(KJE);
+				import(`./Dinkytown/Site/KJESiteSpecific.js`).then(() => {
+					import(`./Dinkytown/Core/${calculatorName}.js`).then(() => {
+						import(`./Dinkytown/Site/${calculatorName}Params.js`).then(() => {
+							KJE.init();
+						});
 					});
 				});
 			});
-		});
-	}, [calculatorName]);
+		}
+	});
 
 	return (
 		<div id={`dt-${calculatorName}`}>
