375ff5054b186069b2c77570ebb1afef13691dc3
Marketing_Jenkins <mktdev@connexuscu.org>
2023-11-15 14:47:51 -0600

    213 Updates

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -1,3 +1,4 @@
+import { addJSAsset } from "lib/enqueuedFiles";
 import Script from "next/script.js";
 import React, { useEffect } from "react";
 interface Props {
@@ -5,25 +6,88 @@ interface Props {
 }
 
 const Calculator = ({ calculatorName }: Props): JSX.Element => {
-	useEffect(() => {
-		if(!window.KJE) {
-			import("./Dinkytown/Core/KJE.js").then(({ KJE }) => {
-				window.KJE = Object(KJE);
-				import(`./Dinkytown/Site/KJESiteSpecific.js`).then(() => {
-					import(`./Dinkytown/Core/${calculatorName}.js`).then(() => {
-						import(`./Dinkytown/Site/${calculatorName}Params.js`).then(() => {
-							KJE.init();
-						});
-					});
-				});
-			});
+	// useEffect(() => {
+	// 	if(!window.KJE) {
+	// 		import("./Dinkytown/Core/KJE.js").then(({ KJE }) => {
+	// 			window.KJE = Object(KJE);
+	// 			import(`./Dinkytown/Site/KJESiteSpecific.js`).then(() => {
+	// 				import(`./Dinkytown/Core/${calculatorName}.js`).then(() => {
+	// 					import(`./Dinkytown/Site/${calculatorName}Params.js`).then(() => {
+	// 						KJE.init();
+	// 					});
+	// 				});
+	// 			});
+	// 		});
+	// 	}
+	// });
+	const jsFiles = [
+		{
+			id: 'KJECore',
+			src: `/wp-content/themes/CXCU/vendors/calculators/KJE.js`
+		},{
+			id: 'KJESiteCore',
+			src: `/wp-content/themes/CXCU/vendors/calculators/KJESiteSpecific.js`
+		},{
+			id: `KJE${calculatorName}Params`,
+			src: `/wp-content/themes/CXCU/vendors/calculators/${calculatorName}.js`
+		},{
+			id: `KJESite${calculatorName}Params`,
+			src: `/wp-content/themes/CXCU/vendors/calculators/${calculatorName}Params.js`,
+			onload: () => {
+				window.KJE.init();
+			}
 		}
-	});
+    ];
+	const loadScript = ({id, src, onload = null}) => {
+		const externalScript = document.createElement("script");
+		// externalScript.onerror = loadError;
+		externalScript.id = id;
+		externalScript.async = false;
+		externalScript.type = "text/javascript";
+		externalScript.onload = onload;
+		// externalScript.setAttribute("crossorigin", "anonymous");
+		document.body.appendChild(externalScript);
+		externalScript.src = src;
+	}
+	useEffect(() => {
+		{jsFiles.map((file) => {
+			loadScript(file);
+		})}
+		// if(window.KJE?.init) {
+		// 	window.KJE?.init();
+		// }
+	}, [])
 
 	return (
-		<div id={`dt-${calculatorName}`}>
-			<div id="KJEAllContent"></div>
-		</div>
+		<>				
+			<div id={`dt-${calculatorName}`}>
+				<div id="KJEAllContent"></div>
+			</div>
+			{
+				// addJSAsset({
+				// 	id: 'KJECore',
+				// 	src: `/wp-content/themes/CXCU/vendors/calculators/KJE.js`,
+				// 	strategy: 'lazyOnload',
+				// 	onLoad: () => {
+				// 		addJSAsset({
+				// 			id: 'KJESiteCore',
+				// 			src: `/wp-content/themes/CXCU/vendors/calculators/KJESiteSpecific.js`,
+				// 			strategy: 'lazyOnload'
+				// 		})
+				// 		addJSAsset({
+				// 			id: `KJE${calculatorName}Params`,
+				// 			src: `/wp-content/themes/CXCU/vendors/calculators/${calculatorName}.js`,
+				// 			strategy: 'lazyOnload'
+				// 		})
+				// 		addJSAsset({
+				// 			id: `KJESite${calculatorName}Params`,
+				// 			src: `/wp-content/themes/CXCU/vendors/calculators/${calculatorName}Params.js`,
+				// 			strategy: 'lazyOnload'
+				// 		})
+				// 	}
+				// })
+			}		
+		</>
 	);
 };
 
