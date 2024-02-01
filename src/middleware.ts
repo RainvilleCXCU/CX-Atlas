e7850065abf1fa66eb1ac5278360bf19360725c9
Marketing_Jenkins <mktdev@connexuscu.org>
2024-02-01 17:09:08 -0600

    277 Updates

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,31 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import { fetchWordPressRedirects } from './utils/redirects';
  
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-//   return NextResponse.redirect(new URL('/home', request.url))
+export async function middleware(request: NextRequest) {
+    const pathname = request.nextUrl.pathname;
+    const redirectData = await fetchWordPressRedirects({type: 'url'}) || [];
+    for(let redirect in redirectData) {
+        const regEx = new RegExp(`${redirectData[redirect].source}`, "g");
+        if(regEx.test(pathname)) {
+            const statusCode = redirectData[redirect].permanent ? 308 : 307;
+            return NextResponse.redirect(new URL(redirectData[redirect].destination, request.url), statusCode)
+        }
+    }
+    
+    // No redirect found, continue without redirecting
+    return NextResponse.next()
 }
- 
+
+const getRedirects = async () => {
+    return await fetchWordPressRedirects({type: 'url'});
+}
 // See "Matching Paths" below to learn more
 // export const config = {
-//   matcher: '/about/:path*',
+//     matcher: [
+//         {
+//             source: '/disclosures/fee-schedule(.*)',
+//         }
+//     ]
 // }
