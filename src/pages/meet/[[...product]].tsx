c64a26c1a556421a3dee2845b286da1c1fc2c252
Marketing_Jenkins <mktdev@connexuscu.org>
2024-02-15 16:41:24 -0600

    27 Updates

--- src/pages/meet/[[...product]].tsx
+++ src/pages/meet/[[...product]].tsx
@@ -1,31 +1,25 @@
-import { getNextServerSideProps, getNextStaticProps } from "@faustjs/next";
+import { getNextServerSideProps } from "@faustjs/next";
 import { client } from "client";
 import { Footer, Header } from "components";
-import { GetServerSidePropsContext, GetStaticPropsContext } from "next";
+import { GetServerSidePropsContext } from "next";
 import Head from "next/head";
-import { useState, useEffect, useContext, useRef } from "react";
 import GTM from "components/ThirdParty/gtm";
-import PageTitle from "components/Blocks/PageTitle";
-import Container from "components/Blocks/Container";
-import Columns from "components/Blocks/Columns";
-import Column from "components/Blocks/Column";
 import HotJar from "components/ThirdParty/hotjar";
 import Qualtrics from "components/ThirdParty/qualtrics";
 import Spectrum from "components/ThirdParty/spectrum";
 import Personyze from "components/ThirdParty/personyze";
 import { parseHtml } from "lib/parser";
-import { useQuery } from "@apollo/client";
-import Scheduler from "components/Salesforce/scheduler";
+import { useRouter } from "next/router";
 
 export default function Page({product, productFilters}) {
-    const { useQuery } = client;    
+    const { useQuery } = client;
     const { generalSettings, widgetSettings } = useQuery();
     const widget = widgetSettings?.scheduler;
     return (
         <>
             <Head>
                 <title>
-                    {`Schedule an Call for ${product} - ${generalSettings.title}`}
+                    {`Book a Call`}
                 </title>
             </Head>
             <GTM />
