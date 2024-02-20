bb5c357667e9b3b9dab05bca662fa76e5132676a
Marketing_Jenkins <mktdev@connexuscu.org>
2024-02-20 16:52:41 -0600

    46 Updates

--- src/pages/api/revalidate.tsx
+++ src/pages/api/revalidate.tsx
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
     // Check for secret to confirm this is a valid request
-    if (req.query.secret !== process.env.FAUST_SECRET_KEY) {
+    if (req.query.secret !== process.env.FAUSTWP_SECRET_KEY) {
       return res.status(401).json({ message: 'Invalid token' })
     }
    
