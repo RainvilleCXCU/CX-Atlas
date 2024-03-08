1e5e48e33f99e931635ffb4e1988d857067e5f58
Marketing_Jenkins <mktdev@connexuscu.org>
2024-03-08 13:15:14 -0600

    312 Updates

--- src/faust.config.js
+++ src/faust.config.js
@@ -6,6 +6,9 @@ import possibleTypes from './possibleTypes.json';
  * @type {import('@faustwp/core').FaustConfig}
  **/
 export default setConfig({
+  wpUrl: process.env.NEXT_PUBLIC_WORDPRESS_URL,
+  apiClientSecret: process.env.FAUST_SECRET_KEY,
+  basePath: '/graphql',
   templates,
   experimentalPlugins: [],
   experimentalToolbar: true,
