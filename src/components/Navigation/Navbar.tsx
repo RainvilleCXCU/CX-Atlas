ca4792e723fc1928da300df29cc37a8c545b3d8a
Marketing_Jenkins <mktdev@connexuscu.org>
2024-03-11 09:28:13 -0500

    318 Updates

--- src/components/Navigation/Navbar.tsx
+++ src/components/Navigation/Navbar.tsx
@@ -56,7 +56,7 @@ function DesktopHeaderNavigation(props: HeaderNavigationProps) {
                                                             {title?.childItems?.nodes?.map((navLink, index) => {
                                                                 return (
                                                                     <li key={`${navLink.id}-${navLink.databaseId}`}>
-                                                                        <Link href={navLink.uri || ''} className='dropdown-item cx-nav__dropdown-item'
+                                                                        <Link prefetch={true} href={navLink.uri || ''} className='dropdown-item cx-nav__dropdown-item'
                                                                             onClick={() => {
                                                                                 setIsNavExpanded(!isNavExpanded);
                                                                                 setNavSelected('');
@@ -85,7 +85,7 @@ function DesktopHeaderNavigation(props: HeaderNavigationProps) {
                                                         {title?.childItems?.nodes?.map((navLink, index) => {
                                                             return (
                                                                 <li key={`item-${navLink.id}-${navLink.databaseId}`}>
-                                                                    <Link href={navLink.uri || ''} className='dropdown-item cx-nav__dropdown-item'
+                                                                    <Link prefetch={true} href={navLink.uri || ''} className='dropdown-item cx-nav__dropdown-item'
                                                                         onClick={() => {
                                                                             setIsNavExpanded(!isNavExpanded);
                                                                             setNavSelected('');
