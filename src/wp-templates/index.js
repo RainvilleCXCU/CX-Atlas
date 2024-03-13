cbb11cd259e48b2fef0224508f6f7374e180af8e
Marketing_Jenkins <mktdev@connexuscu.org>
2024-03-13 14:48:12 -0500

    331 Updates

--- src/wp-templates/index.js
+++ src/wp-templates/index.js
@@ -1,5 +1,4 @@
 import category from './category';
-import tag from './tag';
 import frontPage from './front-page';
 import page from './page';
 import single from './single';
@@ -7,9 +6,8 @@ import archive from './archive';
 
 export default {
   category,
-  tag,
   'front-page': frontPage,
   page,
   single,
-  'archive': archive
+  archive
 };
