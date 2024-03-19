8b2b0017d7b8d335d0a3cbd56e9b213c3aa30f11
Marketing_Jenkins <mktdev@connexuscu.org>
2024-03-19 09:02:11 -0500

    346 Updates

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,10 @@ import { NextResponse } from 'next/server'
  
 export default function middleware(request: NextRequest) {
   const nextUrl = request.nextUrl
+  console.log('PRE SEARCH PARAMS');
+  console.log(nextUrl.searchParams);
   nextUrl.searchParams.delete('wordpressNode')
+  console.log('SEARCH PARAMS');
+  console.log(nextUrl.searchParams);
   return NextResponse.rewrite(nextUrl)
 }
