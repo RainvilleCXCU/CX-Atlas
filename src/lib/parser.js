9fe3e605bba103c7ed3d3c06d773d4a400977387
Marketing_Jenkins <mktdev@connexuscu.org>
2024-04-02 13:08:42 -0500

    98 Updates

--- src/lib/parser.js
+++ src/lib/parser.js
@@ -39,7 +39,7 @@ export const parseHtml = (html) => {
         replace: (element) => {
             const { name, attribs, children } = element;
             // Skip none block elements
-            if(name !== 'a' && name !== 'div' && name !== 'span' && name !== 'button' && name !== 'p' && name !== 'h3') {
+            if(name !== 'a' && name !== 'div' && name !== 'span' && name !== 'button' && name !== 'p' && name !== 'h3' && name !== 'table') {
                 return;
             }
 
@@ -56,22 +56,8 @@ export const parseHtml = (html) => {
                 );
             }
 
-            // CX Calculator Results
-            else if(attribs?.class?.includes('cx-calculator-results')) {
-                return (
-                    <div {...attributesToProps(attribs)}><CXCalcResults>{children}</CXCalcResults></div>
-                )
-            } 
-
-            // CX Calculator
-            else if(attribs?.class?.includes('cx-calculator')) {
-                return (
-                    <div {...attributesToProps(attribs)}><CXCalc>{children}</CXCalc></div>
-                )
-            } 
-
             // Responsive Table
-            else if (name === 'table' && attribs?.class.includes("tablepress-responsive")) {
+            else if (name === 'table' && attribs?.class?.includes("tablepress-responsive")) {
                 return (
                     <div className="cx-table--responsive"><table {...attributesToProps(attribs)}>{domToReact(children, options)}</table></div>
                 )
@@ -112,6 +98,20 @@ export const parseHtml = (html) => {
                     <Disclosure {...attributesToProps(attribs)}>{domToReact(children, options)}</Disclosure>
                 )
             }
+
+            // CX Calculator Results
+            else if(attribs?.class?.includes('cx-calculator-results')) {
+                return (
+                    <div {...attributesToProps(attribs)}><CXCalcResults>{children}</CXCalcResults></div>
+                )
+            } 
+
+            // CX Calculator
+            else if(attribs?.class?.includes('cx-calculator')) {
+                return (
+                    <div {...attributesToProps(attribs)}><CXCalc>{children}</CXCalc></div>
+                )
+            } 
 
             // Datatrac
             else if (attribs?.['data-datatrac-perform']) {
--- src/pages/[...wordpressNode].tsx
+++ src/pages/[...wordpressNode].tsx
@@ -1,4 +1,8 @@
+import { gql, useQuery } from '@apollo/client';
 import { getWordPressProps, WordPressTemplate } from '@faustwp/core';
+import * as MENUS from '../constants/menus';
+import apolloClient from 'apolloClient';
+import { MenuNavigation } from 'components';
 import { getPageNum } from 'utils/urlParser';
 
 export default function Page(props) {
@@ -16,8 +20,32 @@ export function getStaticProps(ctx) {
 }
 
 export async function getStaticPaths() {
+
+  // Get all Paths in Header Navigation
+  const { data } = await apolloClient.query({
+    query: gql`
+    ${MenuNavigation.fragments.entry}
+    query GetMenuItems(
+      $headerLocation: MenuLocationEnum
+    ) {
+    headerMenuItems: menuItems(where: { location: $headerLocation }, first: 255) {
+      nodes {
+        ...NavigationMenuItemFragment
+      }
+    }}`,
+    variables: {
+      headerLocation: MENUS.PRIMARY_LOCATION,
+    }
+  })
+  const paths = data.headerMenuItems.nodes.map(item => {
+    if(item.uri[0] === '/') {
+      return item.uri
+    }
+  }).filter(uri => uri !== undefined && uri !== '/about/branch-and-atm-locations/');
+  paths.push('/rates/');
+  
   return {
-    paths: [],
+    paths: paths,
     fallback: 'blocking',
   };
 }
