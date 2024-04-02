4486db97066811d86ef3e583341800dec3c1cb61
Marketing_Jenkins <mktdev@connexuscu.org>
2024-04-02 13:39:26 -0500

    370 Updates

--- src/pages/[...wordpressNode].tsx
+++ src/pages/[...wordpressNode].tsx
@@ -1,51 +1,107 @@
-import { gql, useQuery } from '@apollo/client';
-import { getWordPressProps, WordPressTemplate } from '@faustwp/core';
-import * as MENUS from '../constants/menus';
-import apolloClient from 'apolloClient';
-import { MenuNavigation } from 'components';
-import { getPageNum } from 'utils/urlParser';
+import { gql, useQuery } from "@apollo/client";
+import { getWordPressProps, WordPressTemplate } from "@faustwp/core";
+import * as MENUS from "../constants/menus";
+import apolloClient from "apolloClient";
+import { MenuNavigation } from "components";
+import { getPageNum } from "utils/urlParser";
 
 export default function Page(props) {
   return <WordPressTemplate {...props} />;
 }
 
 export function getStaticProps(ctx) {
-  return getWordPressProps({ ctx,
-    revalidate: process.env.NEXT_PUBLIC_PAGE_REVALIDATION ? parseInt(process.env.NEXT_PUBLIC_PAGE_REVALIDATION) : null, 
+  return getWordPressProps({
+    ctx,
+    revalidate: process.env.NEXT_PUBLIC_PAGE_REVALIDATION
+      ? parseInt(process.env.NEXT_PUBLIC_PAGE_REVALIDATION)
+      : null,
     extra: {
       query: {
-        page: getPageNum(ctx.params.wordpressNode)
-      }
-    } } );
+        page: getPageNum(ctx.params.wordpressNode),
+      },
+    },
+  });
 }
 
 export async function getStaticPaths() {
-
   // Get all Paths in Header Navigation
-  const { data } = await apolloClient.query({
+  // const { data } = await apolloClient.query({
+  //   query: gql`
+  //   ${MenuNavigation.fragments.entry}
+  //   query GetMenuItems(
+  //     $headerLocation: MenuLocationEnum
+  //   ) {
+  //   headerMenuItems: menuItems(where: { location: $headerLocation }, first: 255) {
+  //     nodes {
+  //       ...NavigationMenuItemFragment
+  //     }
+  //   }}`,
+  //   variables: {
+  //     headerLocation: MENUS.PRIMARY_LOCATION,
+  //   }
+  // })
+  // const paths = data.headerMenuItems.nodes.map(item => {
+  //   if(item.uri[0] === '/') {
+  //     return item.uri
+  //   }
+  // }).filter(uri => uri !== undefined && uri !== '/about/branch-and-atm-locations/');
+  // paths.push('/rates/');
+
+  /**
+   * Pre Render all Pages and Posts
+   */
+
+  const pageData = await apolloClient.query({
     query: gql`
-    ${MenuNavigation.fragments.entry}
-    query GetMenuItems(
-      $headerLocation: MenuLocationEnum
-    ) {
-    headerMenuItems: menuItems(where: { location: $headerLocation }, first: 255) {
-      nodes {
-        ...NavigationMenuItemFragment
+      query GetAllPages {
+        pages(first: 255, where: {status: PUBLISH}) {
+          edges {
+            node {
+              uri
+            }
+          }
+        }
       }
-    }}`,
-    variables: {
-      headerLocation: MENUS.PRIMARY_LOCATION,
+    `,
+  });
+  const postData = await apolloClient.query({
+    query: gql`
+      query GetAllPosts {
+        posts(first: 255, where: {status: PUBLISH}) {
+          edges {
+            node {
+              uri
+            }
+          }
+        }
+      }
+    `,
+  });
+  const pages = pageData.data.pages.nodes
+    .map((item) => {
+      if (item.uri[0] === '/' && item !== '/') {
+        return item.uri;
+      }
+    })
+    .filter(
+      (uri) => uri !== undefined && uri !== "/about/branch-and-atm-locations/"
+    );
+
+  const posts = postData.data.headerMenuItems.nodes
+  .map((item) => {
+    if (item.uri[0] === '/' && item !== '/') {
+      return item.uri;
     }
   })
-  const paths = data.headerMenuItems.nodes.map(item => {
-    if(item.uri[0] === '/') {
-      return item.uri
-    }
-  }).filter(uri => uri !== undefined && uri !== '/about/branch-and-atm-locations/');
-  paths.push('/rates/');
-  
+  .filter(
+    (uri) => uri !== undefined
+  );
+
   return {
-    paths: paths,
-    fallback: 'blocking',
+    paths: [
+      ...pages,
+      ...posts
+    ],
+    fallback: "blocking",
   };
 }
