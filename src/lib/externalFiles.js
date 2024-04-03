0cc24f35b2e52205eb4b780040c7f54e8f68d385
Marketing_Jenkins <mktdev@connexuscu.org>
2024-04-03 15:56:43 -0500

    115 Updates

--- src/lib/externalFiles.js
+++ src/lib/externalFiles.js
@@ -1,9 +1,9 @@
 export const files = {
     css: [
-        {
-            id: 'font',
-            src: `https://cloud.typography.com/6914618/${process.env.CLOUD_FONT || '7711232'}/css/fonts.css`,
-        },
+        // {
+        //     id: 'font',
+        //     src: `https://cloud.typography.com/6914618/${process.env.CLOUD_FONT || '7711232'}/css/fonts.css`,
+        // },
         {
             id: 'genesis-blocks-style',
             src: '/wp-content/plugins/genesis-page-builder/vendor/genesis/blocks/dist/style-blocks.build.css'
