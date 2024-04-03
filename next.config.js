1fed1a7c9e490a0149d0beb2544c58e2ad59fd34
Marketing_Jenkins <mktdev@connexuscu.org>
2024-04-03 13:27:22 -0500

    113 Updates

--- next.config.js
+++ next.config.js
@@ -179,6 +179,16 @@ let nextConfig = {
     },
     trailingSlash: true,
     swcMinify: true,
-    output: 'standalone'
+    webpack: (config, { dev, isServer }) => {
+        if (!dev && !isServer) {
+            Object.assign(config.resolve.alias, {
+            "react/jsx-runtime.js": "preact/compat/jsx-runtime",
+            react: "preact/compat",
+            "react-dom/test-utils": "preact/test-utils",
+            "react-dom": "preact/compat",
+            });
+        }
+        return config;
+    }
 };
 module.exports = withFaust(withBundleAnalyzer(nextConfig));
