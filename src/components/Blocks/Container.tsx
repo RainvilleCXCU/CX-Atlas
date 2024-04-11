8b3d9a69c5c08a9b23ff7a58473d4be0eb4ee75c
Marketing_Jenkins <mktdev@connexuscu.org>
2024-04-11 10:46:13 -0500

    396 Updates

--- src/components/Blocks/Container.tsx
+++ src/components/Blocks/Container.tsx
@@ -4,15 +4,17 @@ export interface Props {
     align?,
     children?
     classNames?
+    style?
 }
 
 function Container({
     align = '',
+    style = {},
     children = <></>,
     classNames
 }: Props): JSX.Element {    
   return (
-    <div className={`wp-block-genesis-blocks-gb-container${ align === 'full' ? ' alignfull' : ''} gb-block-container ${classNames}`}>
+    <div className={`wp-block-genesis-blocks-gb-container${ align === 'full' ? ' alignfull' : ''} gb-block-container ${classNames}`} style={style}>
         <EqualHeight timeout={0} animationSpeed={0}>{children}</EqualHeight>
     </div>
   );
