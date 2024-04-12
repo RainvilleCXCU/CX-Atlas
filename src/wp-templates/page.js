e7452c710ef9b4d83f444c9512c72b7ffd860f65
Marketing_Jenkins <mktdev@connexuscu.org>
2024-04-12 16:09:41 -0500

    156 Updates

--- src/wp-templates/page.js
+++ src/wp-templates/page.js
@@ -2,7 +2,7 @@ import { gql } from '@apollo/client';
 import * as MENUS from '../constants/menus';
 import { BlogInfoFragment } from '../fragments/GeneralSettings';
 import { AlertFragment } from '../fragments/Alerts';
-import { ThirdPartySettingsFragment,HotJar, Qualtrics, Spectrum, Siteimprove } from '../components/ThirdParty';
+import { ThirdPartySettingsFragment, GTM, Personyze, HotJar, Qualtrics, Spectrum, Siteimprove } from '../components/ThirdParty';
 import {
   Header,
   Footer,
@@ -48,16 +48,16 @@ export default function Component(props) {
 				twitter_label1={"Est. reading time"} // Not sure where this is in the page object
 				twitter_data1={seo?.readingTime + " minutes"}
   />
-			{/* <GTM
+			<GTM
         id={gtmId}
-        enabled={gtmEnabled} /> */}
+        enabled={gtmEnabled} />
       <HotJar
         id={hotjarId}
         enabled={hotjarEnabled} />
-			{/* <Personyze
+			<Personyze
         id={personyzeId}
         enabled={personyzeEnabled}
-        domains={personyzeDomains} /> */}
+        domains={personyzeDomains} />
         
       {
         activeAlerts.length > 0 &&
