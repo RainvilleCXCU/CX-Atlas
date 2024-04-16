4949eb316edbe613a214f4ffcac90308a8b1e24a
Marketing_Jenkins <mktdev@connexuscu.org>
2024-04-16 10:59:05 -0500

    163 Updates

--- src/lib/enqueuedFiles.js
+++ src/lib/enqueuedFiles.js
@@ -2,7 +2,7 @@ import Script from 'next/script';
 
 export const addCSSAsset = asset => {
   if (asset.src !== null && asset.src !== undefined) {
-    return (<link rel="stylesheet" href={asset.src.replace(process.env.NEXT_PUBLIC_WORDPRESS_URL, '')} key={asset.id} id={asset.id} />)
+    return (<link rel="stylesheet" href={asset.src} key={asset.id} id={asset.id} />)
   }
 }
 export const addJSAsset = asset => {
