d20467fc5d20096a396fc606167b7d717e284902
Marketing_Jenkins <mktdev@connexuscu.org>
2024-04-19 08:26:15 -0500

    408 Updates

--- src/components/Header/SearchBar.tsx
+++ src/components/Header/SearchBar.tsx
@@ -4,9 +4,10 @@ import { useRouter } from 'next/router';
 import { Store } from 'context/store';
 
 interface SearchBarProps {
-	device?: string,
-	navOpen?: boolean,
+	device?: string
+	navOpen?: boolean
 	setNavOpen?
+	showSearch?: boolean
 	logo?
 }
 
@@ -71,7 +72,7 @@ function DesktopSearchBar(props: SearchBarProps) {
 function MobileSearchBar(props: SearchBarProps) {
 	const [isSearchExpanded, setIsSearchExpanded] = useState(false);
 	const [searchTerm, setSearchTerm] = useState('');
-	const {navOpen, setNavOpen, logo} = props;
+	const {navOpen, setNavOpen, logo, showSearch } = props;
 	const router = useRouter();
 	const searchRef = useRef(null);
     const [state, setState] = useContext(Store);
@@ -108,38 +109,40 @@ function MobileSearchBar(props: SearchBarProps) {
 
 	return (
 		<>
-			<MobileHeader logo={logo} setSearchTerm={setSearchTerm} isSearchExpanded={isSearchExpanded} setIsSearchExpanded={setIsSearchExpanded} navOpen={navOpen} setNavOpen={setNavOpen} />
-			<div className={`modal cx-modal${isSearchExpanded ? ' show' : ''}`} id="searchModal" aria-labelledby="searchModalLabel" aria-hidden="true">
-				<form className="modal-dialog cx-search-mobile" role="search" action="/" onSubmit={submitSearch}>
-					<div className="modal-content cx-search-mobile__content">
-						<div className={`modal-body cx-search-mobile__body${isSearchExpanded ? ' cx-search-mobile__body--show' : ''}`}>
-							<div className="cx-search-mobile__input">
-								<input type="search" aria-label="Search" defaultValue="" placeholder="Search the site..." id="cxMobileSearch" name="s" ref={searchRef} />
+			<MobileHeader logo={logo} setSearchTerm={setSearchTerm} showNav={showSearch} isSearchExpanded={isSearchExpanded} setIsSearchExpanded={setIsSearchExpanded} navOpen={navOpen} setNavOpen={setNavOpen} />
+			{ showSearch &&
+				<div className={`modal cx-modal${isSearchExpanded ? ' show' : ''}`} id="searchModal" aria-labelledby="searchModalLabel" aria-hidden="true">
+					<form className="modal-dialog cx-search-mobile" role="search" action="/" onSubmit={submitSearch}>
+						<div className="modal-content cx-search-mobile__content">
+							<div className={`modal-body cx-search-mobile__body${isSearchExpanded ? ' cx-search-mobile__body--show' : ''}`}>
+								<div className="cx-search-mobile__input">
+									<input type="search" aria-label="Search" defaultValue="" placeholder="Search the site..." id="cxMobileSearch" name="s" ref={searchRef} />
+								</div>
+								<button type="button" className={`cx-search-mobile__clear${ searchTerm !== '' ? ' show' : ''}`}
+									onClick={() => {
+										setSearchTerm('');
+									}}>
+									clear<span className="visually-hidden"> current search</span>
+								</button>
+								<button type="submit" className="cx-search-mobile__submit">
+									<svg className="cx-search-mobile__icon" aria-hidden="true" focusable="false" width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+										<path d="M19.25 16.8578H18.065L17.645 16.4528C19.115 14.7428 20 12.5228 20 10.1078C20 4.72279 15.635 0.357788 10.25 0.357788C4.865 0.357788 0.5 4.72279 0.5 10.1078C0.5 15.4928 4.865 19.8578 10.25 19.8578C12.665 19.8578 14.885 18.9728 16.595 17.5028L17 17.9228V19.1078L24.5 26.5928L26.735 24.3578L19.25 16.8578ZM10.25 16.8578C6.515 16.8578 3.5 13.8428 3.5 10.1078C3.5 6.37279 6.515 3.35779 10.25 3.35779C13.985 3.35779 17 6.37279 17 10.1078C17 13.8428 13.985 16.8578 10.25 16.8578Z" fill="#323232" />
+									</svg>
+									<span className="visually-hidden">submit search</span>
+								</button>
 							</div>
-							<button type="button" className={`cx-search-mobile__clear${ searchTerm !== '' ? ' show' : ''}`}
-								onClick={() => {
-									setSearchTerm('');
-								}}>
-								clear<span className="visually-hidden"> current search</span>
-							</button>
-							<button type="submit" className="cx-search-mobile__submit">
-								<svg className="cx-search-mobile__icon" aria-hidden="true" focusable="false" width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-									<path d="M19.25 16.8578H18.065L17.645 16.4528C19.115 14.7428 20 12.5228 20 10.1078C20 4.72279 15.635 0.357788 10.25 0.357788C4.865 0.357788 0.5 4.72279 0.5 10.1078C0.5 15.4928 4.865 19.8578 10.25 19.8578C12.665 19.8578 14.885 18.9728 16.595 17.5028L17 17.9228V19.1078L24.5 26.5928L26.735 24.3578L19.25 16.8578ZM10.25 16.8578C6.515 16.8578 3.5 13.8428 3.5 10.1078C3.5 6.37279 6.515 3.35779 10.25 3.35779C13.985 3.35779 17 6.37279 17 10.1078C17 13.8428 13.985 16.8578 10.25 16.8578Z" fill="#323232" />
-								</svg>
-								<span className="visually-hidden">submit search</span>
-							</button>
 						</div>
-					</div>
-				</form>
-			</div>
+					</form>
+				</div>
+			}
 		</>
 	);
 }
 
-export default function SearchBar({ device, navOpen, setNavOpen, logo, children = <></> }) {
+export default function SearchBar({ device, navOpen, setNavOpen, logo, showSearch, children = <></> }) {
 	if (device.toLowerCase() === 'mobile') {
 		return (
-			<MobileSearchBar logo={logo} navOpen={navOpen} setNavOpen={setNavOpen} />
+			<MobileSearchBar logo={logo} navOpen={navOpen} setNavOpen={setNavOpen} showSearch={showSearch} />
 		);
 	}
 	return (
