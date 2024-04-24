7ae40687a087bc72b2e405eb5c3020fde9444db1
Marketing_Jenkins <mktdev@connexuscu.org>
2024-04-24 11:29:43 -0500

    175 Updates

---
+++ src/components/Modal/modal.tsx
@@ -0,0 +1,78 @@
+import { useContext } from "react";
+import { isModalOpenContext, modalContentContext } from "./modalContext";
+
+export interface Props {
+//   title?;
+//   copy?;
+//   continueLink?;
+//   continueText?;
+//   cancelText?;
+  children?;
+  classNames?;
+}
+
+function Modal({
+//   title = 'Title',
+//   copy = 'Copy',
+//   continueLink,
+//   continueText = "Continue",
+//   cancelText = "Cancel",
+  children = <></>,
+  classNames = "",
+}: Props): JSX.Element {
+  const { isModalOpen, setIsModalOpen } = useContext(isModalOpenContext);
+  const { modalContent, setModalContent } = useContext(modalContentContext);
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+  const {
+      title,
+      copy,
+      continueLink,
+      continueText,
+      cancelText,
+      forceAction
+  } = modalContent || {
+    title: 'Title',
+    copy: 'Copy',
+    continueLink: null,
+    continueText: "Continue",
+    cancelText: "Cancel",
+    forceAction: false
+  }
+  return (
+    <>
+        <div className={`cx-container-modal--fixed cx-container-modal--force-close${isModalOpen ? '' : ' cx-modal__hidden'}`}>
+          <div className="cx-container-modal__modal-bg" onClick={forceAction && closeModal}></div>
+          <div className="cx-container-modal__content">
+            <button className="cx-button--close" onClick={closeModal}></button>
+            <span>
+              <h3 className="no-margin--top">{title}</h3>
+              <p>{copy}</p>
+              <div className="cx-container-modal__buttons">
+                {cancelText && (
+                  <a
+                    onClick={closeModal}
+                    className="cx-button cx-button--outlined cx-button--compact cx-button--fullwidth"
+                  >
+                    {cancelText}
+                  </a>
+                )}
+                {continueLink && (
+                  <a
+                    href={continueLink}
+                    target="_blank"
+                    className="cx-button cx-button--compact cx-button--fullwidth"
+                  >
+                    {continueText}
+                  </a>
+                )}
+              </div>
+            </span>
+          </div>
+        </div>
+    </>
+      )
+}
+
+export default Modal;
