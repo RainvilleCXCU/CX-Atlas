75a7960021c502d430fa6e8df76334e2f6e269bb
Marketing_Jenkins <mktdev@connexuscu.org>
2024-04-26 13:17:08 -0500

    424 Updates

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,7 +20,7 @@ class CXDoc extends Document {
             href={`/856897/9F6C645B2367EC4D3.css`}
           />
           <link rel="stylesheet" href="/_next/static/css/styles.css" />
-          <link rel="stylesheet" href={`${process.env.NEXT_PUBLIC_WORDPRESS_URL}/wp-content/themes/CXCU/assets/${
+          <link rel="stylesheet" href={`/wp-content/themes/CXCU/assets/${
                 process.env.NEXT_PUBLIC_styleguideVersion || "latest"
               }/cxcuatlas.css`} />
         </Head>
