cb469028db5ccb51a5a76d8d44a11ead48e6194a
Marketing_Jenkins <mktdev@connexuscu.org>
2024-04-26 10:22:36 -0500

    184 Updates

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,18 +10,16 @@ class CXDoc extends Document {
         <Head>
           <link href='https://www.googletagmanager.com' rel='preconnect' />
           <link href='https://www.googletagmanager.com' rel='dns-prefetch' />
-          <link href='https://cloud.typography.com' rel='preconnect' />
-          <link href='https://cloud.typography.com' rel='dns-prefetch' />
+          {/* <link href='https://cloud.typography.com' rel='preconnect' />
+          <link href='https://cloud.typography.com' rel='dns-prefetch' /> */}
           <link href={process.env.NEXT_PUBLIC_WORDPRESS_URL} rel='preconnect' />
           <link href={process.env.NEXT_PUBLIC_WORDPRESS_URL} rel='dns-prefetch' />
           <link
             rel="stylesheet"
-            href={`https://cloud.typography.com/6914618/${
-              process.env.NEXT_PUBLIC_CLOUD_FONTS || "7711232"
-            }/css/fonts.css`}
+            href={`/856897/9F6C645B2367EC4D3.css`}
           />
           <link rel="stylesheet" href="/_next/static/css/styles.css" />
-          <link rel="stylesheet" href={`/wp-content/themes/CXCU/assets/${
+          <link rel="stylesheet" href={`${process.env.NEXT_PUBLIC_WORDPRESS_URL}/wp-content/themes/CXCU/assets/${
                 process.env.NEXT_PUBLIC_styleguideVersion || "latest"
               }/cxcuatlas.css`} />
         </Head>
