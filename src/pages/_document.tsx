00ac71c65e4b171eb62e99f26445bb36f2e99fcb
Matthew Rainville <118932382+RainvilleCXCU@users.noreply.github.com>
2024-06-07 09:27:19 -0500

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -27,7 +27,7 @@ class CXDoc extends Document {
           <link rel="stylesheet" href="/_next/static/css/styles.css" />
           <link rel="stylesheet" href={`/wp-content/themes/CXCU/assets/${
                 process.env.NEXT_PUBLIC_styleguideVersion || "latest"
-              }/cxcuatlas.css?v=2`} />
+              }/cxcuatlas.css?v=3`} />
         </Head>
         <body>
           <Main />
