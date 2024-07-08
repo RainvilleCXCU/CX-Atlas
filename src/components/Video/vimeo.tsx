237fb353fae6909125463ca0604335f8a2f99401
Marketing_Jenkins <mktdev@connexuscu.org>
2024-07-08 14:50:18 -0500

    480 Updates

--- src/components/Video/vimeo.tsx
+++ src/components/Video/vimeo.tsx
@@ -1,6 +1,5 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { embedVimeo } from 'lib/vimeo';
-
 export interface Props {
 	id: string;
     hasBackground: boolean;
@@ -18,17 +17,44 @@ function Vimeo({
     badge = false,
     responsive = true
 }: Props): JSX.Element {
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    // const embedVimeo = (
+    //     {container}
+    // ) => {
+    //     console.log('VIDEO LOAD');    
+    //     if(Player && !isLoaded && id) {
+    //         const options = {
+    //             id: id,
+    //             background: hasBackground,
+    //             responsive: responsive
+    //         }
+    //         const videoPlayer = new Player(container, options);
+    //         videoPlayer.on('play', function() {
+    //             console.log('Played the video');
+    //         });        
+    //     }
+    // }
+
 	useEffect(() => {
-        embedVimeo({container:`vimeo${id}`});
+        console.log('TRY LOADING VIMEO')
+        if(!isLoaded && id) {
+            embedVimeo({container:`vimeo${id}`,id: id, hasBackground: hasBackground, responsive: responsive});
+            setIsLoaded(true);
+        }
+        return () => {
+    		console.log('Cleanup!');
+            setIsLoaded(false);
+    	}
     }, [id])
 	return (
 		<div id={`vimeo${id}`} className="cx-vimeo__video" 
-            data-vimeo-id={id}
-            data-vimeo-background={hasBackground ? 1 : 0} 
-            data-vimeo-player_id={player_id ? 1 : 0}
-            data-vimeo-app_id={app_id}
-            data-vimeo-badge={badge ? 1 : 0}
-            data-vimeo-responsive={responsive ?? 1}
+            // data-vimeo-id={id}
+            // data-vimeo-background={hasBackground ? 1 : 0} 
+            // data-vimeo-player_id={player_id ? 1 : 0}
+            // data-vimeo-app_id={app_id}
+            // data-vimeo-badge={badge ? 1 : 0}
+            // data-vimeo-responsive={responsive ?? 1}
         ></div>
 	);
 }
