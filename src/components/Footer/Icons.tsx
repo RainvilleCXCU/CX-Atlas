4fd4db91ef11af6d77f64557ede454162508834e
Marketing_Jenkins <mktdev@connexuscu.org>
2024-07-08 11:20:16 -0500

    212 Updates

--- src/components/Footer/Icons.tsx
+++ src/components/Footer/Icons.tsx
@@ -3,12 +3,12 @@ import { parseHtml } from 'lib/parser';
 export default function Icons({ children = <></>, footerSocialIcons, footerAppIcons }) {
     return (
         <div className="cx-footer__icons">
-            <span key="footerSocialIcons">
+            <ul className="cx-footer__social" key="footerSocialIcons" aria-label="Connexus Social Media">
                 {parseHtml(footerSocialIcons ?? '')}
-            </span>
-            <span key="footerAppIcons">
+            </ul>
+            <ul className="cx-footer__apps" key="footerAppIcons" aria-label="Download Connexus App">
                 {parseHtml(footerAppIcons ?? '')}
-            </span>
+            </ul>
         </div>
     )
 };
