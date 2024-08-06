94faeb00f376b06a54107b99735e8e6d0894db0e
Marketing_Jenkins <mktdev@connexuscu.org>
2024-08-06 09:19:15 -0500

    498 Updates

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,33 +3,30 @@ import type { NextRequest } from 'next/server'
 const { fetchWordPressRedirects } = require("./utils/redirects");
  
 export async function middleware(request: NextRequest) {
-  const wpRewrites = await fetchWordPressRedirects({ type: "pass" });
-  const wpRedirects = await fetchWordPressRedirects({ type: "url" });
+  if(!request.nextUrl.searchParams.get('wordpressNode')) {
+    const wpRewrites = await fetchWordPressRedirects({ type: "pass" });
+    const wpRedirects = await fetchWordPressRedirects({ type: "url" });
 
-  const rewrite = wpRewrites.filter(rewritePath => {
-    return request.nextUrl.pathname == rewritePath.source.replace('(.*)', '') ? rewritePath : null
-  });
+    const rewrite = wpRewrites.filter(rewritePath => {
+      return request.nextUrl.pathname == rewritePath.source.replace('(.*)', '') ? rewritePath : null
+    });
 
-  const redirect = wpRedirects.filter(redirectPath => {
-    if(redirectPath.regex) {
-      if(request.nextUrl.pathname.match(redirectPath.source)) {
+    const redirect = wpRedirects.filter(redirectPath => {
+      if(redirectPath.regex) {
+        if(request.nextUrl.pathname.match(redirectPath.source)) {
+          return redirectPath;
+        }
+      } else if(request.nextUrl.pathname == redirectPath.source) {
         return redirectPath;
       }
-    } else if(request.nextUrl.pathname == redirectPath.source) {
-      return redirectPath;
-    }
-  });
-  
-  if(!request.nextUrl.searchParams.get('wordpressNode')) {
+    });
+    
     if(rewrite.length == 1) {
-      console.log('Rewrite')
-      console.log(request.url)
-      console.log(rewrite[0])
       return NextResponse.rewrite(new URL(rewrite[0].destination, request.url))
     }
     if(redirect.length > 0) {
-      console.log('Redirect')
       return NextResponse.redirect(new URL(redirect[0].destination, request.url))
     }
   }
+    
 }
