cc9c26728ddf580911ae78c2b302a8380d16112e
Marketing_Jenkins <mktdev@connexuscu.org>
2024-08-09 12:15:17 -0500

    502 Updates

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,14 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 const { fetchWordPressRedirects } = require("./utils/redirects");
+const { fetchVanityByPath } = require("./utils/vanity");
  
 export async function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
   if(!request.nextUrl.searchParams.get('wordpressNode')) {
     const wpRewrites = await fetchWordPressRedirects({ type: "pass" });
     const wpRedirects = await fetchWordPressRedirects({ type: "url" });
+    const vanityURL = await fetchVanityByPath({path: pathname});
 
     const rewrite = wpRewrites.filter(rewritePath => {
       return request.nextUrl.pathname == rewritePath.source.replace('(.*)', '') ? rewritePath : null
@@ -20,7 +23,13 @@ export async function middleware(request: NextRequest) {
         return redirectPath;
       }
     });
-    
+
+    if(vanityURL) {
+      console.log('VANITY!!')
+      console.log(vanityURL);
+      console.log(pathname)
+      return NextResponse.rewrite(new URL(vanityURL.destinationUrl, request.url))
+    }
     if(rewrite.length == 1) {
       return NextResponse.rewrite(new URL(rewrite[0].destination, request.url))
     }
@@ -32,3 +41,12 @@ export async function middleware(request: NextRequest) {
   }
     
 }
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+};
