f77fce0b07cdcfa95186f568f1291324428dcf5c
Marketing_Jenkins <mktdev@connexuscu.org>
2024-08-29 10:28:24 -0500

    517 Updates

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,19 @@
 import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextRequest } from 'next/server'
 const { fetchWordPressRedirects } = require("./utils/redirects");
 const { fetchVanityByPath } = require("./utils/vanity");
  
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
+  const vanityPathMatch = process.env.NEXT_PUBLIC_VANITY_PATH_MATCH ? new RegExp(process.env.NEXT_PUBLIC_VANITY_PATH_MATCH) : new RegExp('/(disclosures/.*)');
+
+  console.log('MIDDLEWARE');
+  console.log(pathname);
+  console.log(vanityPathMatch);
+
   if(!request.nextUrl.searchParams.get('wordpressNode') && (!process.env.NEXT_PUBLIC_DISABLE_MIDDLEWARE_REDIRECT || process.env.NEXT_PUBLIC_DISABLE_MIDDLEWARE_REDIRECT == 'false')) {
     const wpRewrites = await fetchWordPressRedirects({ type: "pass" });
     const wpRedirects = await fetchWordPressRedirects({ type: "url" });
-    const vanityURL = await fetchVanityByPath({path: pathname});
 
     const rewrite = wpRewrites.filter(rewritePath => {
       return request.nextUrl.pathname == rewritePath.source.replace('(.*)', '') ? rewritePath : null
@@ -23,10 +28,6 @@ export async function middleware(request: NextRequest) {
         return redirectPath;
       }
     });
-
-    if(vanityURL) {
-      return NextResponse.rewrite(new URL(vanityURL.destinationUrl, request.url))
-    }
     if(rewrite.length == 1) {
       return NextResponse.rewrite(new URL(rewrite[0].destination, request.url))
     }
@@ -36,6 +37,14 @@ export async function middleware(request: NextRequest) {
       })
     }
   }
+
+  if(vanityPathMatch.test(pathname) && (!process.env.NEXT_PUBLIC_DISABLE_MIDDLEWARE_VANITY || process.env.NEXT_PUBLIC_DISABLE_MIDDLEWARE_VANITY == 'false')) {
+    const vanityURL = await fetchVanityByPath({path: pathname});
+    if(vanityURL) {
+      return NextResponse.rewrite(new URL(vanityURL.destinationUrl, request.url))
+    }
+  }
+  return NextResponse.next();
     
 }
 
@@ -44,6 +53,6 @@ export const config = {
     // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    // '/(disclosures/.*)/',
   ],
 };
