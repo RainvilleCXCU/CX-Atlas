b2e582481d1e4e45d98b3a0fa4f1ea14f4a063dd
Digital Services <digitalservices@connexuscu.org>
2025-03-11 21:34:52 +0000

    AWS CodeBuild Updates

--- src/pages/api/revalidate.js
+++ src/pages/api/revalidate.js
@@ -1,23 +1,26 @@
+import { purgePaths } from '@wpengine/edge-cache';
+
 export default async function handler(req, res) {
-    // Check for a secret token to authenticate the request
-    if (req.query.secret !== process.env.REVALIDATE_SECRET_TOKEN) {
-      return res.status(401).json({ message: 'Invalid token' });
-    }
-  
-    const path = req.query.path;
-  
-    // Ensure the path parameter is provided
-    if (!path) {
-      return res.status(400).json({ message: 'Path query parameter is required' });
-    }
-  
-    try {
-      // Revalidate the specified path
-      console.log('Revalidate');
-      console.log(path);
-      await res.revalidate(path);
-      return res.json({ revalidated: true });
-    } catch (err) {
-      return res.status(500).json({ message: 'Error revalidating', error: err.message });
-    }
+  // Check for a secret token to authenticate the request
+  if (req.query.secret !== process.env.REVALIDATE_SECRET_TOKEN) {
+    return res.status(401).json({ message: 'Invalid token' });
   }
+
+  const path = req.query.path;
+
+  // Ensure the path parameter is provided
+  if (!path) {
+    return res.status(400).json({ message: 'Path query parameter is required' });
+  }
+
+  try {
+    // Revalidate the specified path
+    console.log('Revalidate');
+    console.log(path);
+    await res.revalidate(path);
+    await purgePaths(path);
+    return res.json({ revalidated: true });
+  } catch (err) {
+    return res.status(500).json({ message: 'Error revalidating', error: err.message });
+  }
+}
