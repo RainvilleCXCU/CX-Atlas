1403076a8fdb83da17aea307a9008cc41977dc4d
Digital Services <digitalservices@connexuscu.org>
2025-05-14 19:11:44 +0000

    AWS CodeBuild Updates

---
+++ src/components/Datatrac/BarComparison.tsx
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+
+function DataTracBarComparison({...attribs}) {
+
+  useEffect(() => {
+      // Create the Observer and add the '.show' class when element is in viewport
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+          }
+      });
+    });
+
+    // Grab all of the bar graph elements
+    const rateBarElements = document.querySelectorAll('.rate_info__rate--bar');
+
+    // Tell the Observer to watch the elements
+    rateBarElements.forEach((element) => observer.observe(element));
+  }, []);
+  
+
+  return <div {...attribs}></div>;
+}
+
+export default DataTracBarComparison;
